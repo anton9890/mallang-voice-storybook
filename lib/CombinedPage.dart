e614ecfde422730e067137c7bf90704ecebc4a18
EUNHEE <zzzni1025@gmail.com>
2024-02-13 12:01:46 +0900

    commit1-eh

--- lib/CombinedPage.dart
+++ lib/CombinedPage.dart
@@ -11,107 +11,206 @@ class TermsOfService extends StatefulWidget {
 }
 
 class _TermsOfServiceState extends State<TermsOfService> {
-  // 각 체크박스의 상태를 저장할 맵
-  Map<int, bool> checkboxes = {0: false, 1: false, 2: false, 3: false, 4: false};
+  bool allAgreed = false;
+  bool serviceAgreed = false;
+  bool privacyAgreed = false;
+  bool dataCollectionAgreed = false;
+  bool marketingAgreed = false;
 
-  // 상단 메뉴를 구성하는 위젯
-  Widget _topMenu() {
-    // 가로 방향으로 배열
-    return Wrap(
-      children: [
-        _menuOne(menu: '전체 약관 모두 동의', index: 0),
-        _menuOne(menu: '서비스 이용약관 동의(필수)', index: 1),
-        _menuOne(menu: '개인정보 수집 및 이용동의(필수)', index: 2),
-        _menuOne(menu: '품질 향상을 위한 이용자 데이터 수집(선택)', index: 3),
-        _menuOne(menu: '마케팅 정보 수신 동의(선택)', index: 4),
-      ],
-    );
-  }
-
-  // _menuOne 항목을 구성하는 위젯
-  Widget _menuOne({required String menu, required int index}) {
-    // 사용자의 탭 동작을 처리하는 위젯
-    return GestureDetector(
-      onTap: () {
-        // 체크박스를 클릭하면 상태를 토글
-        setState(() {
-          if (index ==0){
-            // 첫 번째 체크박스를 클릭했을 때
-            bool newValue = !checkboxes[index]!;
-            checkboxes = {for (var key in checkboxes.keys) key: newValue!}; // 모든 체크박스 활성화
-          }
-          else {
-            checkboxes[index] = !checkboxes[index]!;
-          }
-        });
-      },
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            // 체크박스 생성
-            Checkbox(
-              value: checkboxes[index],
-              onChanged: (bool? value) {
-                setState(() {
-                  if (index == 0) {
-                    // 첫 번째 체크박스를 클릭했을 때
-                    checkboxes = {for (var key in checkboxes.keys) key: value!};  // 모든 체크박스 활성화
-                  }
-                  else{
-                    checkboxes[index] = value!;
-                  }
-                });
-              },
-              activeColor: Colors.black, // 체크박스 활성화시 색상
+  // 이는 앱 화면을 빌드하는 메서드입니다.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            '약관 동의',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
-            SizedBox(width: 8.0), // 체크박스와 텍스트 사이의 간격
-            Expanded(
-              child: Text(
-                menu,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: checkboxes[index]!
-                      ? FontWeight.bold
-                      : FontWeight.normal,
-                ),
+          ),
+        ),
+        // 화면의 메인 부분은 여러 텍스트와 체크박스로 구성되어 있습니다.
+        // 각 체크박스는 해당 약관에 동의했는지 여부를 나타내는 불린 변수와 연결되어 있습니다.
+        body: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Center(
+            child: Column(
+              children: [
+                Text('말랑에 오신 것을 환영합니다!',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),),
+                SizedBox(height: 10),
+                Text('말랑 서비스를 이용하기 전에 먼저 이용약관에 동의해주세요.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey.withOpacity(0.5),
+                  ),),
+                SizedBox(height: 70),
+                // '전체 약관 동의' 체크박스는 별도의 Contatiner에 포함되어 있습니다.
+                _buildCheckboxWithContainer('전체 약관 동의', allAgreed, _handleAllAgreed),
+                SizedBox(height: 20,),
+                // 나머지 체크박스들은 Column 내에 포함되어 있습니다.
+                _buildTermsCheckboxes(),
+              ],
+            ),
+          ),
+        ),
+        // 하단에는 '다음' 버튼이 있습니다.
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(20),
+          child: ElevatedButton(
+            onPressed: () {
+              // '전체 약관 동의'가 선택되었거나
+              // '서비스 이용약관 동의'와 '개인정보 수집 및 이용동의'가 모두 선택되었는지 확인합니다.
+              if (allAgreed || (serviceAgreed && privacyAgreed)) {
+                // 조건이 충족되었다면 다음 페이지로 이동합니다.
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+              } else {
+                // 조건이 충족되지 않았다면 경고 메시지를 보여줍니다.
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Text('모든 필수 약관에 동의해주세요.',
+                        textAlign: TextAlign.center,),
+                      actions: <Widget>[
+                        Align(
+                          alignment: Alignment.center,
+                          child: TextButton(
+                            child: Text('확인',
+                              style: TextStyle(
+                                  color: Colors.black
+                              ),),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ),
+                      ],
+                      backgroundColor: Colors.white,
+                    );
+                  },
+                );
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              backgroundColor: Colors.white38,
+              surfaceTintColor: Colors.white38,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
             ),
-          ],
+            child: Text('다음', style: TextStyle(fontSize: 16, color: Colors.white)),
+          ),
         ),
       ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('말랑 서비스 약관 동의'),
+  // 이는 주어진 제목, 상태, 그리고 변경 처리 함수를 가진 체크박스를 빌드합니다.
+  // 이 체크박스는 별도의 Container에 포함되어 있습니다.
+  Widget _buildCheckboxWithContainer(String title, bool value, ValueChanged<bool?> onChanged) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.blueGrey.withOpacity(0.05),
       ),
-      body: Column(
+      child: CheckboxListTile(
+        value: value,
+        onChanged: onChanged,
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: value ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+        controlAffinity: ListTileControlAffinity.leading,
+        activeColor: Colors.black,
+      ),
+    );
+  }
+
+  // 이는 주어진 제목, 상태, 그리고 변경 처리 함수를 가진 체크박스를 빌드합니다.
+  Widget _buildCheckbox(String title, bool value, ValueChanged<bool?> onChanged) {
+    return CheckboxListTile(
+      value: value,
+      onChanged: onChanged,
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: value ? FontWeight.bold : FontWeight.normal,
+        ),
+      ),
+      controlAffinity: ListTileControlAffinity.leading,
+      activeColor: Colors.black,
+    );
+  }
+
+  // 이는 각각의 체크박스들을 빌드합니다.
+  Widget _buildTermsCheckboxes() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      padding: EdgeInsets.all(5),
+      child: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(padding: const EdgeInsets.all(10), child: _topMenu()),
-            ),
-          ),
-          Container(
-            height: 50.0,  // 컨테이너의 높이를 100픽셀로 설정
-            width: double.infinity, // 컨테이너 가로길이를 꽉 차게 설정
-            color: Colors.black,
-            child: TextButton(
-              child: const Text('동의하고 계속하기', style: TextStyle(color: Colors.white),
-              ),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
-              },
-            ),
-          ),
+          _buildCheckbox(
+              '[필수] 서비스 이용약관 동의', serviceAgreed, _handleServiceAgreed),
+          SizedBox(height: 20),
+          _buildCheckbox(
+              '[필수] 개인정보 수집 및 이용동의', privacyAgreed, _handlePrivacyAgreed),
+          SizedBox(height: 20),
+          _buildCheckbox('[선택] 품질 향상을 위한 이용자 데이터 수집', dataCollectionAgreed,
+              _handleDataCollectionAgreed),
+          SizedBox(height: 20),
+          _buildCheckbox(
+              '[선택] 마케팅 정보 수신 동의', marketingAgreed, _handleMarketingAgreed),
         ],
       ),
     );
+  }
+
+  // 이는 각 체크박스의 상태 변화를 처리하는 메서드들입니다.
+  void _handleAllAgreed(bool? value) {
+    setState(() {
+      allAgreed = value!;
+      serviceAgreed = value;
+      privacyAgreed = value;
+      dataCollectionAgreed = value;
+      marketingAgreed = value;
+    });
+  }
+
+  void _handleServiceAgreed(bool? value) {
+    setState(() {
+      serviceAgreed = value!;
+    });
+  }
+
+  void _handlePrivacyAgreed(bool? value) {
+    setState(() {
+      privacyAgreed = value!;
+    });
+  }
+
+  void _handleDataCollectionAgreed(bool? value) {
+    setState(() {
+      dataCollectionAgreed = value!;
+    });
+  }
+
+  void _handleMarketingAgreed(bool? value) {
+    setState(() {
+      marketingAgreed = value!;
+    });
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,34 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:dashed_stepper/dashed_stepper.dart';
+import 'home.dart';
 
-// SignUpPage 클래서 정의
 class SignUpPage extends StatefulWidget {
   @override
   _SignUpPageState createState() => _SignUpPageState();
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  // _form키는 폼의 유효성을 검사하기 위한 키
-  // 이름, 이메일, 비밀번호, 비밀번호 확인, 성별 ..
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // 현재 스텝을 나타내는 변수
+  int _currentStep = 1;
+
+  // 각 step마다 사용할 Form 위젯의 GlobalKey
+  final GlobalKey<FormState> _formKey1 = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey2 = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey3 = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey4 = GlobalKey<FormState>();
+
+  // 각 입력 필드에 대한 컨트롤러 생성
   final _usernameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
   final _phonenumberController = TextEditingController();
+  final _birthdayController = TextEditingController();
   List<bool> _genderSelection = [false, false];
 
-  // 회원가입 폼 위젯
-  // Column 위젯 안에 TextFormField와 Text 위젯을 사용하여 개인정보를 입력받기
-  // TextFormField에는 controller를 설정하여 입력된 값을 저장하고, validator를 설정하여 입력값의 유효성 검사
-  Widget _formWidget() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          '회원가입',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              // DashedStepper 사용하여 회원가입 단계 표시
+              child: DashedStepper(
+                indicatorColor: Colors.grey,
+                length: 3,
+                dotSize: 15,
+                lineHeight: 4,
+                labels: ['개인정보 입력','회원정보 입력','관심분야 설정'],
+                step: _currentStep,
+              ),
+            ),
+            //회원가입 1~4 단계의 페이지 보여주기
+            if (_currentStep == 1)
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: _formKey1,
+                  child: _formWidgetFirstPage(),
+                ),
+              ),
+            if (_currentStep == 2)
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: _formKey2,
+                  child: _formWidgetSecondPage(),
+                ),
+              ),
+            if (_currentStep == 3)
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: _formKey3,
+                  child: _formWidgetThirdPage(),
+                ),
+              ),
+            if (_currentStep == 4)
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: _formKey4,
+                  child: _formWidgetLastPage(),
+                ),
+              ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Visibility(
+              // 현재단계가 1이나 4이면 이전 버튼 안보이게 하기
+              visible: _currentStep != 1 && _currentStep != 4, // _currentStep이 1이 아닐 때만 '이전' 버튼을 표시
+              child: TextButton(
+                onPressed: _currentStep > 1
+                    ? () => setState(() => _currentStep -= 1)
+                    : null,
+                child: const Text('이전'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                bool isValid = false; // 유효성 검사 결과를 저장할 변수
+                // 현재 단계에 따라 해당 Form 위젯의 유효성 검사를 수행합니다.
+                switch (_currentStep) {
+                  case 1:
+                    isValid = _formKey1.currentState!.validate();
+                    break;
+                  case 2:
+                    isValid = _formKey2.currentState!.validate();
+                    break;
+                  case 3:
+                    isValid = _formKey3.currentState!.validate();
+                    break;
+                  case 4:
+                    isValid = _formKey4.currentState!.validate();
+                    break;
+                }
+                // 모든 필드가 유효하다면 다음 단계로 이동합니다.
+                if (isValid && _currentStep < 4) {
+                  setState(() => _currentStep += 1);
+                }
+                else if (_currentStep == 4){
+                  // _currentStep이 4일 때 '완료' 버튼이 눌렸을 때 MainPage()로 이동
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(title: '',)),
+                  );
+                }
+              },
+              child: _currentStep == 4 ? Text('완료') : Text('다음'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _formWidgetFirstPage() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text('이름',
+        Text(
+          '이름',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,
+          ),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
         TextFormField(
           controller: _usernameController,
           decoration: InputDecoration(
@@ -42,12 +171,37 @@ class _SignUpPageState extends State<SignUpPage> {
             return null;
           },
         ),
-        SizedBox(height: 35),
-        Text('성별',
+        SizedBox(height: 20),
+        Text(
+          '생년원일',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,
+          ),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
+        TextFormField(
+          controller: _birthdayController,
+          decoration: InputDecoration(
+            hintText: '생년월일 8자리 (YYYYMMDD)',
+            border: OutlineInputBorder(),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '생년월일을 다시 입력해주세요.';
+            }
+            return null;
+          },
+        ),
+        SizedBox(height: 20),
+        Text(
+          '성별',
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+          textAlign: TextAlign.left,
+        ),
+        SizedBox(height: 10,),
         ToggleButtons(
           children: <Widget>[
             Center(child: Text('남성')),
@@ -62,19 +216,22 @@ class _SignUpPageState extends State<SignUpPage> {
           },
           isSelected: _genderSelection,
           constraints: BoxConstraints(
-            minWidth: (MediaQuery.of(context).size.width - 43) / 2,
             minHeight: 55,
+            minWidth: MediaQuery.of(context).size.width / 2 - 21.5,
           ),
           borderRadius: BorderRadius.circular(3.5),
           fillColor: Colors.grey,
           selectedColor: Colors.white,
         ),
-        SizedBox(height: 35),
-        Text('휴대폰 번호',
+        SizedBox(height: 20),
+        Text(
+          '휴대폰 번호',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,
+          ),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
         Row(
           children: [
             Expanded(
@@ -93,7 +250,7 @@ class _SignUpPageState extends State<SignUpPage> {
                 },
               ),
             ),
-            SizedBox(width: 8), // 각 칸 사이의 간격을 조절하
+            SizedBox(width: 8),
             Expanded(
               child: TextFormField(
                 keyboardType: TextInputType.phone,
@@ -127,12 +284,20 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
           ],
         ),
-        SizedBox(height: 35),
+      ],
+    );
+  }
+
+  Widget _formWidgetSecondPage() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
         Text('이메일',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
         TextFormField(
           controller: _emailController,
           decoration: InputDecoration(
@@ -155,9 +320,10 @@ class _SignUpPageState extends State<SignUpPage> {
         SizedBox(height: 35),
         Text('비밀번호',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
         TextFormField(
           controller: _passwordController,
           obscureText: true,
@@ -175,9 +341,10 @@ class _SignUpPageState extends State<SignUpPage> {
         SizedBox(height: 35),
         Text('비밀번호 확인',
           style: TextStyle(
-            fontSize: 18.0,),
+            fontSize: 16.0,),
           textAlign: TextAlign.left,
         ),
+        SizedBox(height: 10,),
         TextFormField(
           controller: _confirmPasswordController,
           obscureText: true,
@@ -198,45 +365,40 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-  // build 메서드는 위젯의 UI를 구성
-  // Scaffold 위젯을 사용하여 앱의 기본 구조 생성
-  // Padding으로 폼 주위에 여백을 추가
-  // SingleChildScrollView는 스크롤 가능한 컨텐츠를 담을 수 있는 위젯
-  // Form 위젯은 폼을 생성하고 _formKey를 설정하여 폼의 유효성 검사를 수행
-  // ElevatedButton은 회원가입 버튼을 생성하고, 유효성 검사를 통과한 경우에만 누를 수 있도록 함
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('말랑 회원가입'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                _formWidget(), // Expanded를 제거했습니다.
-                SizedBox(height: 35),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 50), // width and height
-                    primary: Colors.black,
-                  ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // 회원가입 로직 수행
-                    }
-                  },
-                  child: Text('회원가입', style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
+  Widget _formWidgetThirdPage() {
+    return Text('관심분야를 설정해주세요!');
+  }
+
+  Widget _formWidgetLastPage() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 200, // 이미지의 너비 조정
+            height: 200, // 이미지의 높이 조정
+            child: Image.asset('assets/images/ballon.png'),
           ),
-        ),
+          SizedBox(height: 20), // 이미지와 텍스트 사이의 간격 조정
+          Text(
+            '환영합니다!',
+            style: TextStyle(fontSize: 20), // 텍스트의 크기 조정
+          ),
+          Text(
+            '가입이 완료되었습니다.',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20,),
+          Text(
+            '로그인하고 말랑 서비스를 이용해보세요 :)',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey.withOpacity(0.5),
+            ),
+          )
+        ],
       ),
     );
   }
+
 }
