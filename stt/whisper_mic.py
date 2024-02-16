3b0498ab1dfe7349ccf32475fd7a65781f975c9a
LeeSJ <98650288+pincesslucy@users.noreply.github.com>
2024-02-16 14:07:53 +0900

    Add files via upload

--- stt/whisper_mic.py
+++ stt/whisper_mic.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 import torch
 import numpy as np
 import time
+import re
 
 r = sr.Recognizer()
 r.energy_threshold = 200
@@ -14,23 +15,29 @@ colors = [
     "\033[38;5;226m", "\033[38;5;190m", "\033[38;5;154m", "\033[38;5;118m", "\033[38;5;82m",
 ]
 
+def wer(ref, pred):
+    ref = re.sub(r"[^ㄱ-ㅣ가-힣\s]", "", ref)
+    pred = re.sub(r"[^ㄱ-ㅣ가-힣\s]", "", pred)
+    r = ref.split()
+    p = pred.split()
+    
+    error_count = 0
+    shift = 0
+    output = []
+    for i in range(len(p)):
+        if shift+i >= len(p)-1:
+            break
+        if i >= len(r):
+            break
+        if r[i] != p[i+shift]:
+            error_count += 1
+            if ref.find(p[i+shift])==-1:
+                output.append(p[i+shift])
+            if r[i] == p[i+shift+1]:
+                shift += 1
 
-def listen(audio_model):
-        print("무슨 말이라도? : ")
-        with sr.Microphone(sample_rate=16000) as source:
-            audio = r.listen(source)
-            audio_data = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0).to("cuda")
-            #record inference time
-            start = time.time()
-            text = audio_model.transcribe(audio_data, fp16=False)["text"]
-            end = time.time()
-            inference_time = end - start
-            #save wav file
-            audio_wav = audio.get_wav_data()
-            with open("audio.wav", "wb") as f:
-                f.write(audio_wav)
-            score = get_score("audio.wav")
-            return text, inference_time, score, audio_wav
+            
+    return error_count, round((1 - (error_count/len(r)))*100, 1), output
 
 def get_score(audio):
 
@@ -72,15 +79,16 @@ def get_score(audio):
         word_scores.append(result.token_probs[start:end])
         start = end
     # 평균 점수가 0.5 이하인 단어만 선택
-    low_score_words = [{'word': word, 'worst': [char for i, char in enumerate(word) if word_score[i] <= 0.4]} for word, word_score in zip(words, word_scores) if sum(word_score)/len(word_score) <= 0.5]
-    print("error_output: ", low_score_words)
-    return low_score_words
+    low_score_words = [{'word': word, 'worst': [char for i, char in enumerate(word) if word_score[i] <= 0.4]} for word, word_score in zip(words, word_scores) if len(word_score) > 0 and sum(word_score)/len(word_score) <= 0.5 and len(word) > 1]
 
-run_once = True
-while run_once:
-    my_speech, inference_time, _score, audio_file = listen(audio_model)
-    if my_speech == "종료":
-        break
-    else:
-        print("inference time : ", inference_time)
-    run_once = False
+    print("error_output: ", low_score_words)
+    return result.text, low_score_words
+
+# run_once = True
+# while run_once:
+#     my_speech, inference_time, _score, audio_file = listen(audio_model)
+#     if my_speech == "종료":
+#         break
+#     else:
+#         print("inference time : ", inference_time)
+#     run_once = False
