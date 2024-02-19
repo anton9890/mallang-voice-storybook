2a71dc5387657e55c9e7d88b37c62d16cf1eabfd
lee <anton0909@naver.com>
2024-02-19 15:29:21 +0900

    On branch main Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   .gitignore
    	modified:   tts_test/client.py
    	new file:   tts_test/client_wavs/output.wav
    	modified:   tts_test/mallang_xtts.py
    	new file:   tts_test/requirements.txt
    	modified:   tts_test/tts.py
    	modified:   tts_test/wavs/input.wav
    	modified:   tts_test/wavs/output.wav

--- tts_test/mallang_xtts.py
+++ tts_test/mallang_xtts.py
@@ -18,15 +18,15 @@ def tts(text, temperature=0.3, length_penalty=0.1, repetition_penalty=2.0, speed
     text,
     gpt_cond_latent=gpt_cond_latent,
     speaker_embedding=speaker_embedding,
-    # temperature=temperature, # Add custom parameters here
+    temperature=temperature, # Add custom parameters here
     language="ko",
     length_penalty=length_penalty,
-    # repetition_penalty=repetition_penalty,
-    # speed=speed
+    repetition_penalty=repetition_penalty,
+    speed=speed,
     enable_text_splitting=False,
     )
 
     return torchaudio.save("./tts_test/wavs/output.wav", torch.tensor(out["wav"]).unsqueeze(0), 24000)
 
 #test
-tts("그에 대해 고향사람들인 연변 조선족들이 애착을 갖는 것은 자연스럽다. 하지만 윤동주는 조선의 독립운동가로서 조선의 독립과 자유를 갈구했으며 그의 모든 가족들도 중국 공산화 이후 대한민국으로 이주하여 정착했다는 점에서 현대 한국인들이 그를 자국인으로 간주하는 것은 당연하다. 조선의 독립을 위해서 살았던 그의 정신을 계승한다면 같은 한민족이 그를 존경하는 것은 문제가 되진 않을 것이다. 하지만 중국 당국은 윤동주의 명성에 묻어갈 궁리만 하고 있다.")
+#tts("그에 대해 고향사람들인 연변 조선족들이 애착을 갖는 것은 자연스럽다. 하지만 윤동주는 조선의 독립운동가로서 조선의 독립과 자유를 갈구했으며 그의 모든 가족들도 중국 공산화 이후 대한민국으로 이주하여 정착했다는 점에서 현대 한국인들이 그를 자국인으로 간주하는 것은 당연하다. 조선의 독립을 위해서 살았던 그의 정신을 계승한다면 같은 한민족이 그를 존경하는 것은 문제가 되진 않을 것이다. 하지만 중국 당국은 윤동주의 명성에 묻어갈 궁리만 하고 있다.")
