d5b0d45190569f6ed3f45eb7e37415505e4d58c9
Sumin0411 <kimsumin411@gmail.com>
2024-02-26 19:40:56 +0900

    자장가 모드 & 스크롤 기능 추가

--- lib/Widget/test.dart
+++ lib/Widget/test.dart
@@ -87,7 +87,7 @@ class _TestState extends State<Test> {
                           color: Colors.grey.withOpacity(0.5),
                           spreadRadius: 5,
                           blurRadius: 7,
-                          offset: Offset(0, 3),
+                          offset: const Offset(0, 3),
                         ),
                       ],
                       image: const DecorationImage(
@@ -113,161 +113,212 @@ class _TestState extends State<Test> {
               ),
             ),
             const SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 80.0,
-                        height: 80.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: const Offset(1, 3),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.1),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: const Offset(1, 3),
+                              ),
+                            ],
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/거.png'),
+                              fit: BoxFit.cover,
                             ),
-                          ],
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/거.png'),
-                            fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      AnimatedButton(
-                        child: Text('${turtle ?? "거북이"} 모드',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            color: Colors.black,
-                          ),),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => test(widget.email, widget.title, turtle), // 'rabbit' 값을 전달
-                            ),
-                          );
-                        },
-                        height: 40,
-                        width: 100,
-                        type: null,
-                        color: Colors.white70,
-                        shadowColor: Colors.amber[300],
-                        borderRadius: 30,
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        AnimatedButton(
+                          child: Text('${turtle ?? "거북이"} 모드',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              color: Colors.black,
+                            ),),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => test(widget.email, widget.title, turtle), // 'rabbit' 값을 전달
+                              ),
+                            );
+                          },
+                          height: 40,
+                          width: 100,
+                          type: null,
+                          color: Colors.white70,
+                          shadowColor: Colors.amber[300],
+                          borderRadius: 30,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 80.0,
-                        height: 80.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.1),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/토.png'),
+                              fit: BoxFit.cover,
                             ),
-                          ],
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/토.png'),
-                            fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      AnimatedButton(
-                        child: Text('${rabbit ?? "토끼"} 모드',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            color: Colors.black,
-                          ),),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => test(widget.email, widget.title, rabbit), // 'rabbit' 값을 전달
-                            ),
-                          );
-                        },
-                        height: 40,
-                        width: 100,
-                        type: null,
-                        color: Colors.white70,
-                        shadowColor: Colors.amber[300],
-                        borderRadius: 30,
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        AnimatedButton(
+                          child: Text('${rabbit ?? "토끼"} 모드',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              color: Colors.black,
+                            ),),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => test(widget.email, widget.title, rabbit), // 'rabbit' 값을 전달
+                              ),
+                            );
+                          },
+                          height: 40,
+                          width: 100,
+                          type: null,
+                          color: Colors.white70,
+                          shadowColor: Colors.amber[300],
+                          borderRadius: 30,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 80.0,
-                        height: 80.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(1, 3),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.1),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(1, 3),
+                              ),
+                            ],
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/to.png'),
+                              fit: BoxFit.cover,
                             ),
-                          ],
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/to.png'),
-                            fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      AnimatedButton(
-                        child: Text('감상 모드',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            color: Colors.black,
-                          ),),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => test(widget.email, widget.title, parent), // 'rabbit' 값을 전달
-                            ),
-                          );
-                        },
-                        height: 40,
-                        width: 100,
-                        type: null,
-                        color: Colors.white70,
-                        shadowColor: Colors.amber[300],
-                        borderRadius: 30,
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        AnimatedButton(
+                          child: Text('감상 모드',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              color: Colors.black,
+                            ),),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => test(widget.email, widget.title, parent), // 'rabbit' 값을 전달
+                              ),
+                            );
+                          },
+                          height: 40,
+                          width: 100,
+                          type: null,
+                          color: Colors.white70,
+                          shadowColor: Colors.amber[300],
+                          borderRadius: 30,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.1),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: const Offset(1, 3),
+                              ),
+                            ],
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/7.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        AnimatedButton(
+                          child: Text('자장가 모드',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              color: Colors.black,
+                            ),),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => test(widget.email, widget.title, parent), // 'rabbit' 값을 전달
+                              ),
+                            );
+                          },
+                          height: 40,
+                          width: 100,
+                          type: null,
+                          color: Colors.white70,
+                          shadowColor: Colors.amber[300],
+                          borderRadius: 30,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-            SizedBox(height: 12,),
+            const SizedBox(height: 12,),
             Container(
               width: 380.0,
               decoration: BoxDecoration(
                 color: Colors.orange[100],
-                borderRadius: BorderRadius.circular(25.0), // 모든 모서리를 둥글게 만듭니다.
+                borderRadius: BorderRadius.circular(25.0),
               ),
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
@@ -276,7 +327,7 @@ class _TestState extends State<Test> {
                   children: [
                     Text(
                       '${info}',
-                      style: TextStyle(fontSize: 16, fontFamily: 'Pretendard', color: Colors.black),
+                      style: const TextStyle(fontSize: 16, fontFamily: 'Pretendard', color: Colors.black),
                     ),
                   ],
                 ),
