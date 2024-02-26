23029b75d7245f274733b71a83ef8801d152c7e9
Sumin0411 <kimsumin411@gmail.com>
2024-02-26 19:46:58 +0900

    스크롤 추가

--- lib/Widget/mypage.dart
+++ lib/Widget/mypage.dart
@@ -30,104 +30,106 @@ class MyPage extends StatelessWidget {
             } else if (snapshot.hasError) {
               return Text('Error: ${snapshot.error}');
             } else {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const CircleAvatar(
-                        radius: 40.0,
-                        backgroundImage: AssetImage('assets/images/마이페이지토끼.png'),
-                      ),
-                      const SizedBox(width: 16.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${snapshot.data!['name']} 님',
-                            style: const TextStyle(
-                              fontFamily: 'Pretendard',
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 40.0,
+                          backgroundImage: AssetImage('assets/images/마이페이지토끼.png'),
+                        ),
+                        const SizedBox(width: 16.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${snapshot.data!['name']} 님',
+                              style: const TextStyle(
+                                fontFamily: 'Pretendard',
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '$email',
-                            style: const TextStyle(
-                              fontFamily:  'Pretendard',
-                              fontSize: 16.0,
-                              color: Colors.grey,
+                            Text(
+                              '$email',
+                              style: const TextStyle(
+                                fontFamily:  'Pretendard',
+                                fontSize: 16.0,
+                                color: Colors.grey,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20.0),
-                  Divider(thickness: 1.0),
-                  const SizedBox(height: 20.0),
-                  const Text(
-                    '내 데이터 관리',
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+                          ],
+                        ),
+                      ],
                     ),
-                  ),
-                  const ListTile(
-                    leading: Icon(Icons.payment),
-                    title: Text('녹음 기록',
-                      style: TextStyle(
-                          fontFamily: 'Pretendard'
-                      ),
-                    ),
-                  ),
-                  const ListTile(
-                    leading: Icon(Icons.account_balance_wallet),
-                    title: Text('분석 내역',
-                      style: TextStyle(
-                          fontFamily: 'Pretendard'
-                      ),
-                    ),
-                  ),
-                  const ListTile(
-                    leading: Icon(Icons.credit_card),
-                    title: Text('개인정보 수정',
-                      style: TextStyle(
-                          fontFamily: 'Pretendard'
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  const Divider(thickness: 1.0),
-                  const SizedBox(height: 20.0),
-                  const Text(
-                    '알림 설정',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SwitchListTile(
-                    title: const Text('푸시 알림',
+                    const SizedBox(height: 20.0),
+                    const Divider(thickness: 1.0),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      '내 데이터 관리',
                       style: TextStyle(
                         fontFamily: 'Pretendard',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    value: true,
-                    onChanged: (bool value) {},
-                  ),
-                  SwitchListTile(
-                    title: const Text('이메일 알림',
+                    const ListTile(
+                      leading: Icon(Icons.payment),
+                      title: Text('녹음 기록',
+                        style: TextStyle(
+                            fontFamily: 'Pretendard'
+                        ),
+                      ),
+                    ),
+                    const ListTile(
+                      leading: Icon(Icons.account_balance_wallet),
+                      title: Text('분석 내역',
+                        style: TextStyle(
+                            fontFamily: 'Pretendard'
+                        ),
+                      ),
+                    ),
+                    const ListTile(
+                      leading: Icon(Icons.credit_card),
+                      title: Text('개인정보 수정',
+                        style: TextStyle(
+                            fontFamily: 'Pretendard'
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    const Divider(thickness: 1.0),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      '알림 설정',
                       style: TextStyle(
-                          fontFamily: 'Pretendard'
+                        fontSize: 20.0,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    value: false,
-                    onChanged: (bool value) {},
-                  ),
-                ],
+                    SwitchListTile(
+                      title: const Text('푸시 알림',
+                        style: TextStyle(
+                          fontFamily: 'Pretendard',
+                        ),
+                      ),
+                      value: true,
+                      onChanged: (bool value) {},
+                    ),
+                    SwitchListTile(
+                      title: const Text('이메일 알림',
+                        style: TextStyle(
+                            fontFamily: 'Pretendard'
+                        ),
+                      ),
+                      value: false,
+                      onChanged: (bool value) {},
+                    ),
+                  ],
+                ),
               );
             }
           },
