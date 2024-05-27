1d88b077eed67015d7221d7a659f4472faa2f01a
eunhee <zzzni1025@gmail.com>
2024-05-27 21:42:24 +0900

    05-27커밋

--- lib/Widget/BrandNew.dart
+++ lib/Widget/BrandNew.dart
@@ -8,7 +8,8 @@ class brandnewbook extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('신간도서',
+        title: const Text(
+          '신간도서',
           style: TextStyle(
             fontFamily: 'Moebius',
           ),
@@ -16,46 +17,44 @@ class brandnewbook extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildBookImage('assets/images/끝말잇기.png'),
-                  _buildBookImage('assets/images/나의 빛나는 친구.png'),
-                  _buildBookImage('assets/images/너는 어떤 씨앗이니.png'),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildBookImage('assets/images/여기는 토끼아파트입니다.png'),
-                  _buildBookImage('assets/images/친구의전설.png'),
-                  _buildBookImage('assets/images/클림트의 정원으로.png'),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildBookImage('assets/images/도와줘,브루너.png'),
-                  _buildBookImage('assets/images/봄날의즐거운모자대회.png'),
-                  _buildBookImage('assets/images/변신요가.png'),
-                ],
-              ),
-              const SizedBox(height: 20), // 사진과 버튼 사이 간격 조정
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildBookImage('assets/images/티니핑.png'),
-                  _buildBookImage('assets/images/호박목욕탕.png'),
-                  _buildBookImage('assets/images/홀짝홀짝호로록.png'),
-                ],
-              ),
-              const SizedBox(height: 20), // 스크롤의 마지막 간격
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildBookImage('assets/images/끝말잇기.png'),
+                _buildBookImage('assets/images/나의 빛나는 친구.png'),
+                _buildBookImage('assets/images/너는 어떤 씨앗이니.png'),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildBookImage('assets/images/여기는 토끼아파트입니다.png'),
+                _buildBookImage('assets/images/친구의전설.png'),
+                _buildBookImage('assets/images/클림트의 정원으로.png'),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildBookImage('assets/images/도와줘,브루너.png'),
+                _buildBookImage('assets/images/봄날의즐거운모자대회.png'),
+                _buildBookImage('assets/images/변신요가.png'),
+              ],
+            ),
+            const SizedBox(height: 20), // 사진과 버튼 사이 간격 조정
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildBookImage('assets/images/티니핑.png'),
+                _buildBookImage('assets/images/호박목욕탕.png'),
+                _buildBookImage('assets/images/홀짝홀짝호로록.png'),
+              ],
+            ),
+            const SizedBox(height: 20), // 스크롤의 마지막 간격
           ]),
         ),
       ),
